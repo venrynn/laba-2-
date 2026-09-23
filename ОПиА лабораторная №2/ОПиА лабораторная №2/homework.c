0dac9d700b13c05682589827adf9886969afcf58
vadik <vadimgolovin850@gmail.com>
2026-09-23 18:16:39 +0300

    Add files via upload

---
+++ ОПиА лабораторная №2/ОПиА лабораторная №2/homework.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <locale.h>
+main()
+{
+	setlocale(LC_ALL, "RUS");
+	int x = 67; //стоимость метра
+	int S = 1500; //площадь парусов
+	int L = 42; //ширина твани
+	
+	//расчет длины ткани
+	float dlina = S / (L / 100.);
+
+	//расчет стоимости ткани
+	float stoimost = dlina * x;
+
+	//форматирование вывода
+	printf("Выпишем известные данные \n\nСтоимость метра: %d золотых\nПлощадь парусов: %d м2\nШирина ткани: %d см\n\n", x, S, L);
+	printf("Длина ткани равна: %d / (%d / 100) = %.2f\n\n", S, L,dlina);
+	printf("Стоимость за всю ткань равна: %.2f * %d = %.1f\n\n", dlina, x, stoimost);
+	printf("Ответ: %.1f золотых\n", stoimost);
+
+	return 0;
+}
---
+++ ОПиА лабораторная №2/ОПиА лабораторная №2/task 3.c
@@ -0,0 +1,8 @@
+﻿#include <stdio.h>
+#include <locale.h>
+main()
+{
+	int n = 3, m = 1333;
+	setlocale(LC_ALL, "RUS");
+	printf("Дано: \n%8d \n%8d \n\n   --------\nОтвет: \n%+010.5f",n,m,n*1./1333);
+}
